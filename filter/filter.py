db21f681860a28ec93d4fb925fc805f51635e528
Ming <125284928+MingLin1995@users.noreply.github.com>
2023-09-23 22:57:32 +0800

    Add files via upload

---
+++ filter/filter.py
@@ -0,0 +1,232 @@
+import aiohttp
+import asyncio
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+BASE_URL = "https://fapi.binance.com/fapi/v1"
+
+intervals = []
+
+
+@app.route('/add_intervals', methods=['POST'])
+def add_intervals_route():
+    data = request.json
+    intervals.extend(data)
+    # print(intervals)
+    asyncio.run(main(data))
+
+    return jsonify({"message": "Intervals added successfully"})
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template("index.html")
+
+
+# 成交量
+
+
+async def fetch_volume(session):
+    volume_url = f"{BASE_URL}/ticker/24hr"
+    try:
+        async with session.get(volume_url) as response:
+            if response.status == 200:
+                volume_info = await response.json()
+                sorted_volume_info = sorted(volume_info, key=lambda volume: float(
+                    volume.get("quoteVolume")), reverse=True)
+                return sorted_volume_info
+            else:
+                return None
+    except aiohttp.ClientError as e:
+        print(f"Error: {e}")
+        return None
+
+
+# 判斷單位數
+
+
+def format_volume(volume):
+    if volume >= 100000000:
+        volume_units = "億"
+        volume_formatted = volume / 100000000
+    else:
+        volume_units = "萬"
+        volume_formatted = volume / 10000
+    formatted_volume = "{:,.2f}".format(volume_formatted)  # 成交量取到小數點第二位
+    return f"{formatted_volume}{volume_units}"
+
+# 取得K線資料
+
+
+async def fetch_klines(session, symbol, time_interval):
+    klines_url = f"{BASE_URL}/klines"
+    params = {
+        "symbol": symbol,
+        "interval": time_interval,
+        "limit": 365,
+    }
+    response = await session.get(klines_url, params=params)
+    klines_data = await response.json()
+    return [float(entry[4]) for entry in klines_data]
+
+# 計算均線
+
+
+class MovingAverageCalculator:
+    def __init__(self, period):
+        self.period = period
+        self.data = []
+        self.cumulative_sum = 0.0
+
+    def add_data_point(self, value):
+        self.data.append(value)
+        self.cumulative_sum += value
+        # 如果資料數量>時框，就扣掉最舊的一筆資料
+        if len(self.data) > self.period:
+            self.cumulative_sum -= self.data.pop(0)
+
+    def get_moving_average(self):
+        if len(self.data) < self.period:
+            return None
+        return self.cumulative_sum / self.period
+
+
+async def process_symbol(session, symbol, time_interval, param_1, param_2, comparison_operator_1, param_3, param_4, comparison_operator_2, logical_operator):
+    klines_data = await fetch_klines(session, symbol, time_interval)
+    ma_1 = MovingAverageCalculator(param_1)
+    ma_2 = MovingAverageCalculator(param_2)
+
+    if param_3 is not None and param_4 is not None:
+        ma_3 = MovingAverageCalculator(param_3)
+        ma_4 = MovingAverageCalculator(param_4)
+
+    for value in klines_data:
+        ma_1.add_data_point(value)
+        ma_2.add_data_point(value)
+        if param_3 is not None and param_4 is not None:
+            ma_3.add_data_point(value)
+            ma_4.add_data_point(value)
+
+    ma_1_value = round(ma_1.get_moving_average(),
+                       7) if ma_1.get_moving_average() is not None else None
+    ma_2_value = round(ma_2.get_moving_average(),
+                       7) if ma_2.get_moving_average() is not None else None
+    if param_3 is not None and param_4 is not None:
+        ma_3_value = round(ma_3.get_moving_average(),
+                           7) if ma_3.get_moving_average() is not None else None
+        ma_4_value = round(ma_4.get_moving_average(),
+                           7) if ma_4.get_moving_average() is not None else None
+
+    # 沒被執行所以等於None
+    ma_3_value = None
+    ma_4_value = None
+
+    if ma_1_value is not None and ma_2_value is not None:
+        if ma_3_value is not None or ma_4_value is not None:
+            condition = f"{ma_1_value} {comparison_operator_1} {ma_2_value} {logical_operator} {ma_3_value} {comparison_operator_2} {ma_4_value}"
+        else:
+            condition = f"{ma_1_value} {comparison_operator_1} {ma_2_value}"
+        try:
+            if eval(condition):
+                return symbol
+        except Exception as e:
+            print(f"Error evaluating condition: {e}")
+
+    return None
+
+
+async def apply_filter_parallel(session, time_interval, param_1, param_2, param_3, param_4, comparison_operator_1, comparison_operator_2, logical_operator, symbols):
+    tasks = [
+        process_symbol(session, symbol, time_interval, param_1, param_2, comparison_operator_1, param_3, param_4,
+                       comparison_operator_2, logical_operator)
+        for symbol in symbols
+    ]
+    results = await asyncio.gather(*tasks)
+    return [symbol for symbol in results if symbol is not None]
+
+
+async def main(data):
+    async with aiohttp.ClientSession() as session:
+        intervals = data
+        """  [
+            {
+                "time_interval": "15m",
+                "param_1": 25,
+                "param_2": 60,
+                "param_3": None,
+                "param_4": 99,
+                "comparison_operator_1": ">",
+                "comparison_operator_2": ">",
+                "logical_operator": "and"
+            },
+            {
+                "time_interval": "1h",
+                "param_1": 25,
+                "param_2": 60,
+                "param_3": 60,
+                "param_4": 99,
+                "comparison_operator_1": ">",
+                "comparison_operator_2": ">",
+                "logical_operator": "and"
+            },
+            {
+                "time_interval": "4h",
+                "param_1": 25,
+                "param_2": 60,
+                "param_3": 60,
+                "param_4": None,
+                "comparison_operator_1": ">",
+                "comparison_operator_2": ">",
+                "logical_operator": "and"
+            },
+            {
+                "time_interval": "1d",
+                "param_1": None,
+                "param_2": 60,
+                "param_3": None,
+                "param_4": 99,
+                "comparison_operator_1": ">",
+                "comparison_operator_2": ">",
+                "logical_operator": "and"
+            }
+        ] """
+
+        sorted_volume_info = await fetch_volume(session)
+        symbols = [entry["symbol"] for entry in sorted_volume_info]
+        selected_symbols = None
+
+        for i, interval in enumerate(intervals, start=1):
+            time_interval = interval.get("time_interval")
+            param_1 = interval.get("param_1")
+            param_2 = interval.get("param_2")
+            param_3 = interval.get("param_3")
+            param_4 = interval.get("param_4")
+            comparison_operator_1 = interval.get("comparison_operator_1")
+            comparison_operator_2 = interval.get("comparison_operator_2")
+            logical_operator = interval.get("logical_operator")
+
+            if param_1 is None:
+                continue
+
+            if selected_symbols is None:
+                selected_symbols = await apply_filter_parallel(session, time_interval, param_1, param_2, param_3, param_4, comparison_operator_1, comparison_operator_2, logical_operator, symbols)
+            else:
+                selected_symbols = await apply_filter_parallel(session, time_interval, param_1, param_2, param_3, param_4, comparison_operator_1, comparison_operator_2, logical_operator, selected_symbols)
+
+        symbol_volume_data = []
+
+        for symbol in selected_symbols:
+            for entry in sorted_volume_info:
+                if entry["symbol"] == symbol:
+                    symbol_data = {"標的": symbol, "成交量": format_volume(
+                        float(entry["quoteVolume"]))}
+                    symbol_volume_data.append(symbol_data)
+                    break
+
+        print(symbol_volume_data)
+        print(len(symbol_volume_data), "筆資料")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
---
+++ filter/static/index.js
@@ -0,0 +1,155 @@
+/* function processForm() {
+  //第一組參數
+  const timeInterval_1 = document.getElementById("time-interval-1").value;
+  const MA1_1 = document.getElementById("MA1-1").value;
+  const comparisonOperator1_1 = document.getElementById(
+    "comparison-operator-1-1"
+  ).value;
+  const MA1_2 = document.getElementById("MA1-2").value;
+  const logicalOperator_1 = document.getElementById("logical-operator-1").value;
+  const MA1_3 = document.getElementById("MA1-3").value;
+  const comparisonOperator1_2 = document.getElementById(
+    "comparison-operator-1-2"
+  ).value;
+  const MA1_4 = document.getElementById("MA1-4").value;
+  //第二組參數
+  const timeInterval_2 = document.getElementById("time-interval-2").value;
+  const MA2_1 = document.getElementById("MA2-1").value;
+  const comparisonOperator2_1 = document.getElementById(
+    "comparison-operator-2-1"
+  ).value;
+  const MA2_2 = document.getElementById("MA2-2").value;
+  const logicalOperator_2 = document.getElementById("logical-operator-2").value;
+  const MA2_3 = document.getElementById("MA2-3").value;
+  const comparisonOperator2_2 = document.getElementById(
+    "comparison-operator-2-2"
+  ).value;
+  const MA2_4 = document.getElementById("MA2-4").value;
+  //第三組參數
+  const timeInterval_3 = document.getElementById("time-interval-3").value;
+  const MA3_1 = document.getElementById("MA3-1").value;
+  const comparisonOperator3_1 = document.getElementById(
+    "comparison-operator-3-1"
+  ).value;
+  const MA3_2 = document.getElementById("MA3-2").value;
+  const logicalOperator_3 = document.getElementById("logical-operator-3").value;
+  const MA3_3 = document.getElementById("MA3-3").value;
+  const comparisonOperator3_2 = document.getElementById(
+    "comparison-operator-3-2"
+  ).value;
+  const MA3_4 = document.getElementById("MA3-4").value;
+  //第四組參數
+  const timeInterval_4 = document.getElementById("time-interval-4").value;
+  const MA4_1 = document.getElementById("MA4-1").value;
+  const comparisonOperator4_1 = document.getElementById(
+    "comparison-operator-4-1"
+  ).value;
+  const MA4_2 = document.getElementById("MA4-2").value;
+  const logicalOperator_4 = document.getElementById("logical-operator-4").value;
+  const MA4_3 = document.getElementById("MA4-3").value;
+  const comparisonOperator4_2 = document.getElementById(
+    "comparison-operator-4-2"
+  ).value;
+  const MA4_4 = document.getElementById("MA4-4").value;
+
+  console.log("第一组参数：");
+  console.log("時間週期：" + timeInterval_1);
+  console.log("MA參數一：" + MA1_1);
+  console.log("比較運算符一：" + comparisonOperator1_1);
+  console.log("MA參數二：" + MA1_2);
+  console.log("邏輯運算符：" + logicalOperator_1);
+  console.log("MA參數三：" + MA1_3);
+  console.log("比較運算符二：" + comparisonOperator1_2);
+  console.log("MA參數四：" + MA1_4);
+
+  console.log("第二组参数：");
+  console.log("時間週期：" + timeInterval_2);
+  console.log("MA參數一：" + MA2_1);
+  console.log("比較運算符一：" + comparisonOperator2_1);
+  console.log("MA參數二：" + MA2_2);
+  console.log("邏輯運算符：" + logicalOperator_2);
+  console.log("MA參數三：" + MA2_3);
+  console.log("比較運算符二：" + comparisonOperator2_2);
+  console.log("MA參數四：" + MA2_4);
+
+  console.log("第三组参数：");
+  console.log("時間週期：" + timeInterval_3);
+  console.log("MA參數一：" + MA3_1);
+  console.log("比較運算符一：" + comparisonOperator3_1);
+  console.log("MA參數二：" + MA3_2);
+  console.log("邏輯運算符：" + logicalOperator_3);
+  console.log("MA參數三：" + MA3_3);
+  console.log("比較運算符二：" + comparisonOperator3_2);
+  console.log("MA參數四：" + MA3_4);
+
+  console.log("第四组参数：");
+  console.log("時間週期：" + timeInterval_4);
+  console.log("MA參數一：" + MA4_1);
+  console.log("比較運算符一：" + comparisonOperator4_1);
+  console.log("MA參數二：" + MA4_2);
+  console.log("邏輯運算符：" + logicalOperator_4);
+  console.log("MA參數三：" + MA4_3);
+  console.log("比較運算符二：" + comparisonOperator4_2);
+  console.log("MA參數四：" + MA4_4);
+} */
+
+function processForm() {
+  const intervalData = [];
+
+  const parameterGroups = 4;
+
+  for (let i = 1; i <= parameterGroups; i++) {
+    const timeInterval = document.getElementById(`time-interval-${i}`).value;
+    const maParameters = [];
+    const comparisonOperator = [];
+    const logicalOperator = document.getElementById(
+      `logical-operator-${i}`
+    ).value;
+
+    for (let j = 1; j <= 4; j++) {
+      const maParamValue = document.getElementById(`MA${i}-${j}`).value;
+      // 轉換為整數，否則為null
+      const maParam = maParamValue ? parseInt(maParamValue) : null;
+      maParameters.push({
+        value: maParam,
+      });
+    }
+
+    for (let j = 1; j <= 2; j++) {
+      comparisonOperator.push({
+        comparisonOperator: document.getElementById(
+          `comparison-operator-${i}-${j}`
+        ).value,
+      });
+    }
+
+    intervalData.push({
+      time_interval: timeInterval,
+      param_1: maParameters[0].value,
+      param_2: maParameters[1].value,
+      param_3: maParameters[2].value,
+      param_4: maParameters[3].value,
+      comparison_operator_1: comparisonOperator[0].comparisonOperator,
+      comparison_operator_2: comparisonOperator[1].comparisonOperator,
+      logical_operator: logicalOperator,
+    });
+
+    console.log(intervalData);
+  }
+
+  fetch("/add_intervals", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(intervalData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // 处理后端的响应
+      console.log(data.message);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
